5724c9bcd876d445d8a64b75af10c9aaa0446147
syed <syed@digitribe.in>
2018-12-18 14:31:44 +0530

    lists exercise completed

--- basic/list2.py
+++ basic/list2.py
@@ -14,14 +14,7 @@
 # modify the passed in list.
 def remove_adjacent(nums):
   # +++your code here+++
-  result = []
-  i = 0
-  for i in range(len(nums)):
-    if nums[i] == nums[i+1]: 
-      nums.remove(nums[i])
-      result.append(nums)
-      print(result)
-    i = i + 1        
+  result = list(set(nums))  
   return result
 
 
@@ -31,7 +24,9 @@ def remove_adjacent(nums):
 # pass of both lists.
 def linear_merge(list1, list2):
   # +++your code here+++
-  return
+  merged_list = list1+list2
+  sorted_merg_list = sorted(merged_list)
+  return sorted_merg_list
 
 # Note: the solution above is kind of cute, but unforunately list.pop(0)
 # is not constant time with the standard python list implementation, so
